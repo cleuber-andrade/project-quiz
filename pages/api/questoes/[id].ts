8bc5a6445e819f916155cca70ffa7302fa3e5b88
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-27 13:15:37 -0300

    Update [id].ts

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -1,14 +1,14 @@
 import questoes from "../bancoDeQuestoes";
 
-export default (req, res) => {
-  const idSelecionado = +req.query.id
+export default selecaoQuestoes = (req, res) => {
+  const idSelecionado = +req.query.id;
 
   const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado);
 
   if(unicaQuestaoOuNada.length === 1){
     const questaoSelecionada = unicaQuestaoOuNada[0]
-    res.status(200).json(questaoSelecionada.converterParaObjeto())
+    res.status(200).json(questaoSelecionada.converterParaObjeto());
   }else{
-    res.status(204).send()
+    res.status(204).send();
   }
 }
