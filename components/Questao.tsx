e964e2178e3455b38af1354ba8fe8367c6ab12cf
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-28 16:44:56 -0300

    update

--- components/Questao.tsx
+++ components/Questao.tsx
@@ -1,6 +1,7 @@
 import QuestaoModel from "../model/questao";
 import styles from "../styles/Questao.module.css";
 import Enunciado from "./Enunciado";
+import Resposta from "./Resposta";
 
 interface QuestaoProps {
   valor: QuestaoModel 
@@ -9,9 +10,22 @@ interface QuestaoProps {
 export default function Questao(props: QuestaoProps){
   const questao = props.valor;
 
+  function renderizarRespostas(){
+    return questao.resposta.map((resp, i)=> {
+      return <Resposta
+        key={i}
+        valor={resp}
+        indice={i}
+        letra="A"
+        corLetra="#f2c866"
+      /> 
+    })
+  }
+
   return (
     <div className={styles.questao}>
       <Enunciado texto={questao.enunciado}/>
+      {renderizarRespostas()}
     </div>
   )
 }
