b10afd57e83f6f1de75ebfcb40325acedbf99477
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-29 13:58:55 -0300

    Update Resposta.tsx

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -14,16 +14,18 @@ export default function Resposta(props: RespostaProps){
   const resposta = props.valor;
 
   return (
-    <div className={styles.resposta} onClick={() => props.respostaFornecida(props.indice)}>
+    <div className={styles.resposta} onClick={() => props.respostaFornecida(props.indice)}>      
       <div className={styles.conteudoResposta}>
-        {/* <div className={styles.frente}> 
-          <div className={styles.letra} style={{ backgroundColor: props.corFundoLetra }}>          
+      {resposta.revelada ? (
+        <div className={styles.frente}> 
+          <div className={styles.letra} style={{ backgroundColor: props.    corFundoLetra }}>          
             {props.letra}
           </div>
           <div className={styles.valor}>
             {resposta.valor}
           </div>
-        </div> */}
+        </div>
+      ) : (
         <div className={styles.verso}>
           {resposta.certa ? (
             <div className={styles.certa}>
@@ -35,11 +37,9 @@ export default function Resposta(props: RespostaProps){
               <div>A resposta informada está errada...</div>
               <div className={styles.valor}>{resposta.valor}</div>
             </div>
-          )} 
-          
-
-          
+          )}          
         </div>
+      )}       
       </div>
     </div>
   )
