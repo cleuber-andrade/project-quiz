3429d1420924ca4aa210b41604eaa37113d6aeea
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-29 18:44:00 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
   async function carregarQuestao(idQuestao: number) {
     const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`);
     const json = await resp.json();
-    console.log(json)
+    console.log(json.respostas)
   }
 
   useEffect(()=> {
