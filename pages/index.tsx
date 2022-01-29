a5395882f218580607136369658a410fefb85c4d
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-29 18:40:43 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,31 +13,33 @@ const questaoMock = new QuestaoModel(1, "Melhor cor?", [
 const BASE_URL = "http://localhost:3000/api";
 
 export default function Home() {
-  const[idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([]);
-  const [questao, setQuestao] = useState<QuestaoModel>();  
+  const [idsDasQuestoes, setidsDasQuestoes] = useState<number[]>([])
+  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock);
 
-  async function carregarIdsDasQuestoes(){
+  async function carregarIdsDasQuestoes() {
     const resp = await fetch(`${BASE_URL}/questionario`);
     const idsDasQuestoes = await resp.json();
-    setIdsDasQuestoes(idsDasQuestoes);
+    console.log(idsDasQuestoes);
+    setidsDasQuestoes(idsDasQuestoes);
   }
 
-  async function carregarQuestao(idQuestao: number){
+  async function carregarQuestao(idQuestao: number) {
     const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`);
     const json = await resp.json();
-    console.log(json.respostas);    
+    console.log(json)
   }
 
-  useEffect(()=>{
-    carregarIdsDasQuestoes()
+  useEffect(()=> {
+    carregarIdsDasQuestoes();
   },[])
 
-  useEffect(()=>{
-    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0]) 
+  useEffect(()=> {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0]);
   },[idsDasQuestoes])
+  
 
-  function questaoRespondida(questaoRespondida: QuestaoModel){
-    setQuestao(questaoRespondida)
+  function questaoRespondida(){
+    
   }
 
   function irParaProximoPasso() {
