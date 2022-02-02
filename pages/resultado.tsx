f8e05de1863072aa6c86038d91f5ff2bea6c81ae
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-02-02 18:41:31 -0300

    Update resultado.tsx

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Resultado.module.css";
 import { useRouter } from "next/router";
 import Estatistica from "../components/Estatistica";
+import Botao from "../components/Botao";
 
 export default function Resultado(){
   const router = useRouter();
@@ -16,7 +17,8 @@ export default function Resultado(){
         <Estatistica texto="Perguntas" valor={total}/>
         <Estatistica texto="Certas" valor={certas} corFundo="#9cd2a4"/>
         <Estatistica texto="Percentual" valor={`${percentual}`} corFundo="#de6a33"/>    
-      </div>        
+      </div> 
+      <Botao href="/" texto="Tentar novamente"/>       
     </div>
   )
 }
